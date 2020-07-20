17a883a4e9a461c5b02757e82226b9f5e7e3578c
kaustavkarmakar2 <kaustavkarmakar2@gmail.com>
2020-07-20 21:40:41 +0530

    Updates

---
+++ precache-manifest.36c177f43cb512a795c0d6ac0e53b4d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfe2d6674cbe2c1bef934f69a4cfd85",
+    "url": "/Reactmoviedatabaseapp/index.html"
+  },
+  {
+    "revision": "4520bc7bb7737fffac89",
+    "url": "/Reactmoviedatabaseapp/static/css/main.d341e08f.chunk.css"
+  },
+  {
+    "revision": "fe4dec88b4311d4cc60d",
+    "url": "/Reactmoviedatabaseapp/static/js/2.fd08b29f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Reactmoviedatabaseapp/static/js/2.fd08b29f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4520bc7bb7737fffac89",
+    "url": "/Reactmoviedatabaseapp/static/js/main.387e50a4.chunk.js"
+  },
+  {
+    "revision": "35d70d05c274e7d63276",
+    "url": "/Reactmoviedatabaseapp/static/js/runtime-main.9177fe09.js"
+  }
+]);
